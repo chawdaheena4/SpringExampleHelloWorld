8b668cb61d4fd94aaff55e5ba35da9c93d26d954
chawdaheena4 <chawda.heena4@gmail.com>
2017-09-10 19:07:11 +0530

    Added java files

---
+++ src/main/java/com/mkyong/common/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.mkyong.common;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloWorld 
+{
+	public static String name;
+	
+	public static void main(String[] args) {
+		name="perk";
+		System.out.println("Hello World ");
+	}
+	
+	public void printHello() {
+		System.out.println("Hello ! " + name);
+	}
+}
