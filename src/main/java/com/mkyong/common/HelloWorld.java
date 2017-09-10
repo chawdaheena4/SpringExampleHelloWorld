c13d8aa9c7d0606b49751c4f3714af3400864650
chawdaheena4 <chawda.heena4@gmail.com>
2017-09-10 19:25:13 +0530

    commited numberstowords code

--- src/main/java/com/mkyong/common/HelloWorld.java
+++ src/main/java/com/mkyong/common/HelloWorld.java
@@ -4,16 +4,109 @@ package com.mkyong.common;
  * Hello world!
  *
  */
-public class HelloWorld 
+import java.util.Scanner;
+public class HelloWorld  
+ 
 {
-	public static String name;
-	
+    private static final String[] specialNames = {
+        "",
+        " thousand",
+        " million",
+        " billion",
+        " trillion",
+        " quadrillion",
+        " quintillion"
+    };
+    
+    private static final String[] tensNames = {
+        "",
+        " ten",
+        " twenty",
+        " thirty",
+        " forty",
+        " fifty",
+        " sixty",
+        " seventy",
+        " eighty",
+        " ninety"
+    };
+    
+    private static final String[] numNames = {
+        "",
+        " one",
+        " two",
+        " three",
+        " four",
+        " five",
+        " six",
+        " seven",
+        " eight",
+        " nine",
+        " ten",
+        " eleven",
+        " twelve",
+        " thirteen",
+        " fourteen",
+        " fifteen",
+        " sixteen",
+        " seventeen",
+        " eighteen",
+        " nineteen"
+    };
+
+	private static byte[] a;
+    
+    private String convertLessThanOneThousand(int number) {
+        String current;
+        
+        if (number % 100 < 20){
+            current = numNames[number % 100];
+            number /= 100;
+        }
+        else {
+            current = numNames[number % 10];
+            number /= 10;
+            
+            current = tensNames[number % 10] + current;
+            number /= 10;
+        }
+        if (number == 0) return current;
+        return numNames[number] + " hundred" + current;
+    }
+    
+    public String convert(int number) {
+
+        if (number == 0) { return "zero"; }
+        
+        String prefix = "";
+        
+        if (number < 0) {
+            number = -number;
+            prefix = "negative";
+        }
+        
+        String current = "";
+        int place = 0;
+        
+        do {
+            int n = number % 1000;
+            if (n != 0){
+                String s = convertLessThanOneThousand(n);
+                current = s + specialNames[place] + current;
+            }
+            place++;
+            number /= 1000;
+        } while (number > 0);
+        
+        return (prefix + current).trim();
+    }
+    
+    @SuppressWarnings("resource")
 	public static void main(String[] args) {
-		name="perk";
-		System.out.println("Hello World ");
-	}
-	
-	public void printHello() {
-		System.out.println("Hello ! " + name);
-	}
-}
+        HelloWorld obj = new HelloWorld();
+        Scanner i = new Scanner(System.in);
+        int a = i.nextInt();
+        System.out.println("*** " + obj.convert(a));
+        System.out.println("*** " + obj.convert(-55));
+    }
+} 
